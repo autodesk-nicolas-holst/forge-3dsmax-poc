c4fc870d801399de50c54bcb790390efc29f7181
autodesk-nicolas-holst <41784931+autodesk-nicolas-holst@users.noreply.github.com>
2019-02-18 12:14:51 +0000

    Update importfromfbx.py

--- research/importfromfbx.py
+++ research/importfromfbx.py
@@ -5,3 +5,11 @@
 
 # and to make sure we're not adding it to the current scene you would do:
 # resetMaxFile #noprompt
+
+
+# So now for the python equivalents:
+from pymxs import runtime as rt
+rt.resetMaxFile(rt.Name("noPrompt"))
+rt.exportFile("c:/temp/test3.fbx",rt.Name("noPrompt"),using="FBXEXP")
+
+# yeah, so you might have guessed the using parameter, but to go from #noprompt to rt.Name("noPrompt") was non-trivial...
