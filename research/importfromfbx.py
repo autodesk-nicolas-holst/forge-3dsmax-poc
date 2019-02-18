d2f5ff4f7e403a202a7832d9b7c7f28a74ae2c8b
autodesk-nicolas-holst <41784931+autodesk-nicolas-holst@users.noreply.github.com>
2019-02-18 11:42:52 +0000

    Update importfromfbx.py

--- research/importfromfbx.py
+++ research/importfromfbx.py
@@ -2,3 +2,6 @@
 
 # in maxscript you do:
 # importfile "c:/temp/test.fbx" #noPrompt using:FBXIMP
+
+# and to make sure we're not adding it to the current scene you would do:
+# resetMaxFile #noprompt
