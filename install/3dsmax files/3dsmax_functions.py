f82bde81e4cbc6bd3bf599b8eb22530d453e8984
autodesk-nicolas-holst <41784931+autodesk-nicolas-holst@users.noreply.github.com>
2019-02-18 15:24:00 +0000

    Update 3dsmax_functions.py

--- install/3dsmax files/3dsmax_functions.py
+++ install/3dsmax files/3dsmax_functions.py
@@ -1,1 +1,15 @@
 # file for 3ds Max functions
+
+def add_properties(config):
+   # before we upload populate the properties
+   # uuid & name
+   for c in rt.geometry:
+      if rt.getUserPropVal(c,"original_name")==None:
+         rt.setUserPropVal(c,"original_name",c.Name)
+      if rt.getUserPropVal(c,"uuid")==None:
+         rt.setUserPropVal(c,"uuid",uuid.uuid4().hex)
+      # then rename the object so it's uuid+name (checking the this hasn't been done already)
+      c.Name=rt.getUserPropVal(c,"original_name")+rt.getUserPropVal(c,"uuid")
+      
+def restore_name(config):
+  # todo
